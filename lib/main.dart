06cfe5c168132194a9d376e460cbd99430c6ec82
hetbahn <summeracne@gmail.com>
2021-01-11 00:20:42 +0900

    글씨 색넣기

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-    ));
+    )
+  );
 
 class HomePage extends StatefulWidget {
   @override
@@ -20,44 +21,40 @@ class _HomePageState extends State<HomePage> {
         brightness: Brightness.light,
         elevation: 0,
         leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            color: Colors.black87,
-          ),
+          icon: Icon(Icons.menu, color: Colors.black87,),
           onPressed: () {},
         ),
       ),
       body: SafeArea(
-          child: Column(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-            ),
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Find Your',
-                  style: TextStyle(color: Colors.black87, fontSize: 25),
-                ),
-                SizedBox(
-                  height: 3,
-                ),
-                Text(
-                  'Inspiration',
-                  style: TextStyle(color: Colors.black, fontSize: 40),
-                ),
-                Container(
-                  padding: EdgeInsets.a,
-                  child: TextField(),
-                )
-              ],
-            ),
-          )
-        ],
-      )),
-    );
+        child: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Find Your', style: TextStyle(color: Colors.black87, fontSize: 25),),
+                  SizedBox(
+                    height: 3,
+                  ),
+                  Text('Inspiration', style: TextStyle(color: Colors.black, fontSize: 40),),
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(244, 243, 243, 1),
+                      
+                    ),
+                    child: TextField(),
+                  )
+                ],
+              ),
+            )
+          ],
+        )
+      ),
+    )
   }
 }
